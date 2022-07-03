56e55babfea62ab536abca64f20ebf1788d6fa48
matheus charles ramos <98620399+Charlles21@users.noreply.github.com>
2022-07-03 12:51:49 -0300

    Atualizaçao forçada do pom.xml que esta errada
    bugou o projeto do artista por causa do pom do album entao forcei esta atualizaçao

---
+++ album_sony/src/main/java/com/sony/album_sony/controller/AlbumController.java
@@ -0,0 +1,54 @@
+package com.sony.album_sony.controller;
+
+import java.util.List;
+
+import com.sony.album_sony.dto.AlbumDto;
+import com.sony.album_sony.service.AlbumService;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/albuns")
+public class AlbumController {
+
+    @Autowired
+    private AlbumService service;
+
+    @GetMapping
+    public List<AlbumDto> obterAlbuns(){
+        return service.obterTodosOsAlbuns();
+    }
+    
+    @PostMapping
+    public ResponseEntity<AlbumDto> cadastrarAlbum(@RequestBody AlbumDto album){
+        return new ResponseEntity<>(service.cadastrarAlbum(album), HttpStatus.CREATED);
+    } 
+
+    @DeleteMapping("/{id}")
+    public void excluirAlbum(@PathVariable String id){
+        service.excluirAlbum(id);
+    }
+
+    @PutMapping("/{id}")
+    public AlbumDto alterarAlbum(@PathVariable String id, @RequestBody AlbumDto albumAlterar){
+        return service.alterarAlbum(id, albumAlterar);
+    }
+
+    @GetMapping("/nome/{nome}")
+    public AlbumDto obterPorNome(@PathVariable String nome){
+        return service.obterPorNome(nome);
+    }
+
+}
---
+++ album_sony/src/main/java/com/sony/album_sony/service/AlbumServiceImpl.java
@@ -0,0 +1,65 @@
+package com.sony.album_sony.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sony.album_sony.dto.AlbumDto;
+import com.sony.album_sony.model.Album;
+import com.sony.album_sony.repository.AlbumRepository;
+
+@Service
+public class AlbumServiceImpl implements AlbumService {
+    
+
+    @Autowired
+    private AlbumRepository repository;
+
+    private ModelMapper mapper = new ModelMapper();
+    
+    @Override
+    public List<AlbumDto> obterTodosOsAlbuns(){
+        List<Album> albuns = repository.findAll();
+
+        return albuns.stream()
+            .map(a -> mapper.map(a, AlbumDto.class)) 
+            .collect(Collectors.toList());
+            
+    }
+
+    @Override
+    public AlbumDto cadastrarAlbum(AlbumDto album) {
+        Album albumSalvar = mapper.map(album, Album.class);
+        repository.save(albumSalvar);
+        return mapper.map(albumSalvar, AlbumDto.class);
+    }
+
+    @Override
+    public void excluirAlbum(String id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public AlbumDto alterarAlbum(String id, AlbumDto album) {
+        Optional<Album>albumBusca = repository.findById(id);
+
+        if (albumBusca.isPresent()) {
+            Album albumAlterar = mapper.map(album, Album.class);
+            albumAlterar.setId(id);
+            albumAlterar = repository.save(albumAlterar);
+            return mapper.map(albumAlterar, AlbumDto.class);            
+        }
+        return null;
+    }
+
+    @Override
+    public AlbumDto obterPorNome(String nome) {
+        Album album = repository.findByNome(nome);    
+    return mapper.map(album, AlbumDto.class);
+    }
+
+}
